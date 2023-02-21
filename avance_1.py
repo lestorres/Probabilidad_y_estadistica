eac75e19e5cc7d94173cf9b2890e68e578b2a7ac
Kronos2014 <jandres2019@estudiantec.cr>
2023-02-21 12:15:17 -0600

    mas avance

--- avance_1.py
+++ avance_1.py
@@ -55,3 +55,59 @@ print("La cantidad de elementos en la muestra: ", len(lecturas_sensores_temp_lis
 print(lecturas_sensores_temp_ordenada[:10]) ##primeros 10 elementos de la lista ordenada
      
 #print(type(T2[15])) # sabemos que ha este punto es str
+
+
+##_________________Promedio_______________________________________________________
+
+def promedio(lecturas_sensores_temp_ordenada): # se crea una funcion para determinar el promedio
+    return sum(lecturas_sensores_temp_ordenada) / len(lecturas_sensores_temp_ordenada) # se utiliza la funcion sum para sumar cada valor en la lista
+
+    
+print("El promedio de la muestra es: ", (promedio(lecturas_sensores_temp_ordenada)))
+
+
+##____________Mediana___________________________________________________________________
+from statistics import median # se importa la libreria statistics para poder averiguar la mediana
+
+def mediana(lecturas_sensores_temp_ordenada): # funcion para determinar la mediana
+    return (median(lecturas_sensores_temp_ordenada))
+    
+
+print("La mediana de la muestra es: ",(mediana(lecturas_sensores_temp_ordenada)))
+
+
+##_____________Varianza__________________________________________________________________
+
+def varianza(lecturas_sensores_temp_ordenada): # funcion para calcular la varianza
+    promedio(lecturas_sensores_temp_ordenada) #para la varianza debemos utilizar el promedio por eso llamamos la funcion promedio
+    diff = [(v-promedio(lecturas_sensores_temp_ordenada)) for v in lecturas_sensores_temp_ordenada] # a cada valor de la lista se le resta el promedio
+    sqr_diff = [d**2 for d in diff] # a cada resultado de la resta anterior se eleva al cuadrado
+    sum_sqr_diff = sum(sqr_diff) # se suma cada valor luego de que hayan sido elevados al cuadrado
+    varianza = sum_sqr_diff/ (len(lecturas_sensores_temp_ordenada)-1) # los valores sumados son divididos por la cantidad de valores en la lista menos 1 debido a que es una muestra
+    return varianza
+
+
+print ("La varianza de la muestra es: ",varianza(lecturas_sensores_temp_ordenada))
+
+
+
+"""Ejemplo"""
+edades = [5,6,6,7,8]
+promedio(edades)
+print ("Ejemplo Promedio de edades:", promedio(edades))
+varianza(edades)
+print ("Ejemplo Varianza de edades: ", varianza(edades))
+
+
+##___________Desviacion Estandar_________________
+
+def des (lecturas_sensores_temp_ordenada):
+    desviacion = varianza(lecturas_sensores_temp_ordenada)**(1/2)
+    return desviacion
+
+print ("La desviacion estandar de la muestra es: ", des(lecturas_sensores_temp_ordenada))
+
+
+
+
+
