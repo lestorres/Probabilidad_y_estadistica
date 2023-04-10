5fe275e161660f10fc289fef312906caf46f09d5
Lesmes Torres <lesmestorres@estudiantec.cr>
2023-04-09 19:39:16 -0600

    Avance progra

--- proyecto_2/avance_2.py
+++ proyecto_2/avance_2.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-#________________________CASO 1 (MONEDA NORMAL)_______________________________________
+#_________________________________________CASO 1 (MONEDA NORMAL)_______________________________________
 
-###############################################################################################################################
+###########################################################################################################################
 
 #Asignación clásica de la probabilidad de que salga corona en una tirada
 
@@ -18,8 +18,7 @@ p_2 = 0.7       # Probabilidad de que salga CORONA en una tirada (MONEDA TRUCADA
 
 N = 10          # Cantidad total de tiradas de la distribución
 
-#__________________Graficando distribución Geométrica (bastones)________________________________
-
+#_________________________________________________Graficando distribución Geométrica (bastones)_____________________
 def fmp_geometrica(n , p_a): 
    
    k = np.arange(1, n+1)          # Se define la probabilidad de éxito y se crea un array con los valores del 1 al N
@@ -34,21 +33,22 @@ def graficar_geometrica_sintruco():                                # graficar la
     k_g, fmp_geo = fmp_geometrica(N, p_1)
 
     plt.stem(k_g, fmp_geo)                                         # Dibuja los bastones verticales en cada valor de k con una altura igual a la probabilidad (p)
-    plt.title('Distribución Geométrica sin truco')                           # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
-    plt.xlabel('Numero de lanzamientos de moneda (Lanzamientos)')  # Agrega etiqueta al eje x
+    #plt.title('Distribución Geométrica sin truco')                # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
+    plt.xlabel('Número de lanzamientos de moneda (Lanzamientos)')  # Agrega etiqueta al eje x
     plt.ylabel('Probabilidad')                                     # Agrega etiqueta al eje y
     plt.show()                                                     # Mostrar el gráfico
 
-#__________________Graficando distribución Binomial________________________________
+#_________________________________________________Graficando distribución Binomial________________________________
 
-def factorial(n):                                                  #calcular factorial
+def factorial(n):                                                       #calcular factorial
     fact = 1
     for i in range(1, n+1):
         fact *= i
     return fact
  
-def combinatoria(comb_1, comb_2):                                   #calcular combinatoria
+def combinatoria(comb_1, comb_2):                                       #calcular combinatoria
     return factorial(comb_1) / ( factorial(comb_2) * factorial(comb_1-comb_2) )
+
 
 def fmp_binomial( n , p ):
 
@@ -63,8 +63,8 @@ def graficar_binomial_sintruco():
     k_b, fmp_b = fmp_binomial(N, p_1)
 
     plt.stem(k_b , fmp_b )                                           # Dibuja los bastones verticales en cada valor de k con una altura igual a la probabilidad (p)
-    plt.title('Distribución Binomial sin truco')                               # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
-    plt.xlabel('Numero de lanzamientos de moneda (Lanzamientos)')    # Agrega etiqueta al eje x
+    #plt.title('Distribución Binomial sin truco')                    # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
+    plt.xlabel('Número de lanzamientos de moneda (Lanzamientos)')    # Agrega etiqueta al eje x
     plt.ylabel('Probabilidad')                                       # Agrega etiqueta al eje y
     plt.show()                                                       # Mostrarel gráfico
 
@@ -76,11 +76,6 @@ graficar_geometrica_sintruco()
 graficar_binomial_sintruco()
 
 
- 
-
- 
-
-
 ###############################################################################################################################
 #_______________________________________________________Moneda trucada_________________________________________________________
 
@@ -90,8 +85,8 @@ def graficar_geometrica_truco():                                # graficar la di
     k_g, fmp_geo = fmp_geometrica(N, p_2)
 
     plt.stem(k_g, fmp_geo)                                         # Dibuja los bastones verticales en cada valor de k con una altura igual a la probabilidad (p)
-    plt.title('Distribución Geométrica con truco')                           # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
-    plt.xlabel('Numero de lanzamientos de moneda (Lanzamientos)')  # Agrega etiqueta al eje x
+    #plt.title('Distribución Geométrica con truco')                # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
+    plt.xlabel('Número de lanzamientos de moneda (Lanzamientos)')  # Agrega etiqueta al eje x
     plt.ylabel('Probabilidad')                                     # Agrega etiqueta al eje y
     plt.show()                                                     # Mostrar el gráfico
 
@@ -102,37 +97,10 @@ def graficar_binomial_truco():
     k_b, fmp_b = fmp_binomial(N, p_2)
 
     plt.stem(k_b , fmp_b )                                           # Dibuja los bastones verticales en cada valor de k con una altura igual a la probabilidad (p)
-    plt.title('Distribución Binomial con truco')                               # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
-    plt.xlabel('Numero de lanzamientos de moneda (Lanzamientos)')    # Agrega etiqueta al eje x
+    #plt.title('Distribución Binomial con truco')                    # Agrega título al gráfico (NO ES NECESARIA EN INFORME)
+    plt.xlabel('Número de lanzamientos de moneda (Lanzamientos)')    # Agrega etiqueta al eje x
     plt.ylabel('Probabilidad')                                       # Agrega etiqueta al eje y
     plt.show() 
 
 graficar_geometrica_truco()
 graficar_binomial_truco()
-
-
-
-
-
-
-'''
-# Graficando Geométrica (De puntos)
-
-
-def Geometrica(x,p):
-  funcion = ((1-p)**(x-1))*p
-  #print (funcion)
-  return funcion
-x = 10 
-p = 0.5
-Xs = [k for k in range (1,x+1)]
-Ys = [Geometrica(x,p) for x in range(1,x+1)]
-#plt.plot(Xs, Ys, label =f'n = {x}')
-
-plt.title('Distribución Geométrica')
-plt.ylabel('probabilidad')
-plt.xlabel('Número de veces que se tira la moneda')
-plt.plot(Xs,Ys, "o")
-plt.show()
-
-'''
